ccd839646bb52431811f1468d61f08b02c3da486
Latoshia Williams <lswillms@yahoo.com>
2022-09-11 17:50:09 -0500

    commit tree 313e9a5c78e4696608f8d8a7ed75596f5b747f79

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('%c HI', 'color: firebrick')
+let pics = document.getElementById("dog-image-container")
+
+            
+document.addEventListener("DOMContentLoaded", () => {
+    const imgURL = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgURL)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach((imgSrc) => {
+            const dog = document.createElement('img')
+            dog.src = imgSrc;
+            pics.appendChild(dog)
+        })
+    })
+})
+document.addEventListener("load", () => {
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach((dogList) => {
+            let li = document.createElement("li")
+            let container = document.getElementById("dog-breeds")
+            console.log(li.appendChild(dogList))
+            console.log(container.appendChild(li))
+        })
+    })
+})
